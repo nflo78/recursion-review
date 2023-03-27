e8fb48aea26af954b339595c9803235e8a282ca6
Blake Hansen <b.hansen9264@gmail.com>
2023-03-27 16:50:25 -0700

    This is stringify and getElementbyClassname bare minimum requirements completed

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,38 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+
+// We are going to return a list or "array" of elements by a classname
+// we need to recursively traverse the DOM
+//
+var getElementsByClassName = function(className) {
+  // get document body and check if it has the classname
+  // create a result array
+  // if the first test passes push that to array
+
+  var resultArray = [];
+
+ 
+   if ( document.body.classList.contains(className)){
+    resultArray.push(document.body)
+   }
+
+   var recurse  = function(nodelist){
+    for ( var i = 0; i < nodelist.length; i++){
+      if ( nodelist[i].classList !== undefined ) {
+        if ( nodelist[i].classList.contains(className) ) {
+          resultArray.push(nodelist[i]);
+        }
+        if (nodelist[i].childNodes){
+          recurse(nodelist[i].childNodes)
+        }
+      }
+    }
+   }
+
+
+   if ( document.body.childNodes ) {
+    recurse(document.body.childNodes)
+   }
+   return resultArray;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,52 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  // declare result variable
+  var result = '';
+  // unstringifiable test
+  if (typeof obj === 'function') {return;}
+  if (obj === undefined) {return;}
+  // base test
+  if (typeof obj === 'string') {return '"' + obj + '"'}
+  if (typeof obj === 'number') {return obj += ''}
+  if (typeof obj === 'boolean') {return obj += ''}
+  if (obj === null) {return obj += ''}
+  console.log(result)
+  // stringify array
+  if (Array.isArray(obj)) {
+    if (obj.length === 0) {
+      return '[]'
+    }
+    result += '['
+    for (var i = 0; i < obj.length; i++) {
+      result += stringifyJSON(obj[i])
+      if (i < obj.length - 1 && obj.length > 1) {
+        result += ','
+      }
+    }
+    result += ']'
+  }
+  console.log(result)
+  // stringify objects
+  var counter = 0
+if (typeof obj === 'object' && !Array.isArray(obj)) {
+  if (Object.values(obj).length === 0){
+    return '{}'
+  }
+  result += '{'
+  for (var key in obj) {
+    var test = stringifyJSON(key)
+    if (!(typeof obj[key] === 'function') && !(key === 'undefined')) {
+      result += stringifyJSON(key)
+      result += ':'
+      result += stringifyJSON(obj[key])
+      if (Object.values(obj).length > 1 && counter < Object.values(obj).length - 1) {
+        result += ','
+        counter++
+      }
+    }
+    }
+    result +='}'
+}
+  return result;
 };
